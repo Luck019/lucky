c34735c35feffd3af01b0272b9ffd72cafb95cfc
luck019 <luckyarora19@gmail.com>
2021-08-18 08:29:18 +0000

    push script

---
+++ get-metadata.py
@@ -0,0 +1,6 @@
+#!/bin/python2
+import json
+from boto.utils import get_instance_metadata
+
+metadata = get_instance_metadata(timeout=2, num_retries=2)
+print json.dumps(metadata, sort_keys=True,indent=4)
